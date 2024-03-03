e6cfe4d3f7c20a5af0e1b64d8fbff144e1af7a2e
Alvin Pratama <palveeen22@gmail.com>
2024-03-03 12:01:04 +0700

    feat: added server components

---
+++ src/app/register/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <section className='w-full flex justify-center items-center min-h-screen'>
+            <form>
+                <input
+                    type='text'
+                    placeholder='email'
+                />
+            </form>
+        </section>
+    )
+}
+
+export default page
